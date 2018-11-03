f34065db09835799064ea6dd7f3fcfcfe2b064f1
nz-mahmody <44407385+nz-mahmody@users.noreply.github.com>
2018-11-03 15:07:33 +0330

    Add files via upload

---
+++ JavaApplication11.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication11;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author pasargad
+ */
+public class JavaApplication11 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         Scanner d=new Scanner (System.in);
+        float a=d.nextInt();
+        float f=0;
+        f=((9*a)/5)+32;
+        System.out.print(f);
+    }
+    
+}
